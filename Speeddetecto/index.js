b3e04fd3baf56c175a348615160f3d0aed8b1654
iqra <iqralul9@gmail.com>
2024-05-25 16:58:01 +0300

    salaryculator done

---
+++ Speeddetecto/index.js
@@ -0,0 +1,21 @@
+//calculate the speed of the car
+
+let speed = 130;
+let speedLimit = 70;
+let aboveSpeed = 5;
+let overSpeed = 1;
+
+//if the  speed is less than 70 its car
+//else your licences suspend
+
+if (speed <= speedLimit) {
+    console.log("Ok");
+} else {
+    let aboveLimit = speed - speedLimit;
+    let demeritPoints = (aboveLimit / aboveSpeed) * overSpeed;
+    console.log(`DemeritPoints: ${demeritPoints}`);
+
+    while(demeritPoints >= 12){
+        console.log(`License suspended: ${demeritPoints}`);
+    }
+}
